c53a99059ce3a56d07e0d3c58a7c5ef768d50909
Omar Mohamed <om644123@gmail.com>
2023-06-21 21:20:46 +0300

    Apply Register and Login wiht JWT token

---
+++ MagicVilla_VillaAPI/Migrations/20230620225211_Add-LocalUsers-table.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocalUserstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LocalUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalUsers", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2023, 6, 21, 1, 52, 10, 997, DateTimeKind.Local).AddTicks(9337), new DateTime(2023, 6, 21, 1, 52, 10, 997, DateTimeKind.Local).AddTicks(9379) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2023, 6, 21, 1, 52, 10, 997, DateTimeKind.Local).AddTicks(9382), new DateTime(2023, 6, 21, 1, 52, 10, 997, DateTimeKind.Local).AddTicks(9383) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2023, 6, 21, 1, 52, 10, 997, DateTimeKind.Local).AddTicks(9385), new DateTime(2023, 6, 21, 1, 52, 10, 997, DateTimeKind.Local).AddTicks(9386) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2023, 6, 21, 1, 52, 10, 997, DateTimeKind.Local).AddTicks(9388), new DateTime(2023, 6, 21, 1, 52, 10, 997, DateTimeKind.Local).AddTicks(9389) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2023, 6, 21, 1, 52, 10, 997, DateTimeKind.Local).AddTicks(9391), new DateTime(2023, 6, 21, 1, 52, 10, 997, DateTimeKind.Local).AddTicks(9391) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocalUsers");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2023, 4, 26, 20, 12, 48, 534, DateTimeKind.Local).AddTicks(5197), new DateTime(2023, 4, 26, 20, 12, 48, 534, DateTimeKind.Local).AddTicks(5207) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2023, 4, 26, 20, 12, 48, 534, DateTimeKind.Local).AddTicks(5209), new DateTime(2023, 4, 26, 20, 12, 48, 534, DateTimeKind.Local).AddTicks(5209) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2023, 4, 26, 20, 12, 48, 534, DateTimeKind.Local).AddTicks(5211), new DateTime(2023, 4, 26, 20, 12, 48, 534, DateTimeKind.Local).AddTicks(5211) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2023, 4, 26, 20, 12, 48, 534, DateTimeKind.Local).AddTicks(5213), new DateTime(2023, 4, 26, 20, 12, 48, 534, DateTimeKind.Local).AddTicks(5213) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2023, 4, 26, 20, 12, 48, 534, DateTimeKind.Local).AddTicks(5215), new DateTime(2023, 4, 26, 20, 12, 48, 534, DateTimeKind.Local).AddTicks(5215) });
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,82 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private string SecretKey;
+        public UserRepository(ApplicationDbContext db , IConfiguration configuration)
+        {
+            _db = db;
+            SecretKey = configuration.GetValue<string>("ApiSettings:SecretKey");
+        }
+        public bool IsUniqueUser(string username)
+        {
+           var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            var user = await _db.LocalUsers.FirstOrDefaultAsync(x => x.UserName.ToLower() == loginRequestDTO.UserName.ToLower()
+                                                                && x.Password == loginRequestDTO.Password) ;
+            if (user == null)
+            {
+                return new LoginResponseDTO
+                {
+                    Token = string.Empty,
+                    User = null
+                };
+            }
+            // if user foud, we have to generate a JWT token
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key) , SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+            };
+            return loginResponseDTO;
+        }
+
+        public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            LocalUser user = new LocalUser()
+            { 
+                UserName = registerationRequestDTO.UserName,
+                Password = registerationRequestDTO.Password,
+                Name = registerationRequestDTO.Name,
+                Role = registerationRequestDTO.Role
+            };
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
+    }
+}
